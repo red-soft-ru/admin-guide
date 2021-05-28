6f60be91387521194a0e83c7fb446a99497184e0
Artyom Smirnov <artyom.smirnov@red-soft.ru>
2021-05-28 14:15:41 +0300

    Fix version in config

--- source/conf.py
+++ source/conf.py
@@ -18,8 +18,8 @@ import sphinx_rtd_theme
 
 year = datetime.datetime.now().year
 
-version = '5.0'
-release = '5.0.0.0'
+version = '4.0'
+release = '4.0.0.0'
 
 
 # -- Project information -----------------------------------------------------
